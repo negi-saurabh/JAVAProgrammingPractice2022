ac4cdc2ecbc0a602e1667b20f6e649c6e1c225b4
negi-saurabh <saurabh.negi89@gmail.com>
2022-03-08 08:46:31 +0100

    08/Mar

--- Practice/src/hackerRank/Trees/LowestCommonAncestor.java
+++ Practice/src/hackerRank/Trees/LowestCommonAncestor.java
@@ -3,62 +3,56 @@ package hackerRank.Trees;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class LowestCommonAncestor {
-	
-	
-	public static Node lca(Node root, int v1, int v2) {
-      	// Write your code here.
-		
-		if(root==null)
-			return root;
-		
-		 if (root.data == v1 || root.data == v2)
-	            return root;
-		
-		// check left
-		Node valLeftNode = lca(root.left, v1, v2);
-		// check right
-		Node valRightNode = lca(root.right, v1, v2);
-		
-		if (valLeftNode!=null && valRightNode!=null)
-            return root;
- 
-        // Otherwise check if left subtree or right subtree is LCA
-        return (valLeftNode != null) ? valLeftNode : valRightNode;
-		
-    }
-	
-	public static Node insert(Node root, int data) {
-        if(root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if(data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
-    }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        Node root = null;
-        while(t-- > 0) {
-            int data = scan.nextInt();
-            root = insert(root, data);
-        }
-        int v1 = scan.nextInt();
-      	int v2 = scan.nextInt();
-        scan.close();
-        Node ans = lca(root,v1,v2);
-        System.out.println(ans.data);
-    }	
+
+	public static class MyQueue<T> {
+		Stack<T> stackNewestOnTop = new Stack<T>();
+		Stack<T> stackOldestOnTop = new Stack<T>();
+
+		public void enqueue(T value) { // Push onto newest stack
+			stackNewestOnTop.push(value);
+		}
+
+		public T peek() {
+			while(!stackNewestOnTop.isEmpty()) {
+				stackOldestOnTop.push(stackNewestOnTop.peek());
+			}
+			return stackOldestOnTop.peek();
+
+		}
+
+		public T dequeue() {
+			while(!stackNewestOnTop.isEmpty()) {
+				stackOldestOnTop.push(stackNewestOnTop.pop());
+			}
+			
+			return stackOldestOnTop.pop();
+		}
+	}
+
+
+
+	public static void main(String[] args) {
+
+		MyQueue<Integer> queue = new MyQueue<Integer>();
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int operation = scan.nextInt();
+			if (operation == 1) { // enqueue
+				queue.enqueue(scan.nextInt());
+			} else if (operation == 2) { // dequeue
+				queue.dequeue();
+			} else if (operation == 3) { // print/peek
+				System.out.println(queue.peek());
+			}
+		}
+		scan.close();}	
 }
 
 
