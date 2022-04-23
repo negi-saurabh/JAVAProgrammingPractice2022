03b450675bef418f1f286fe56a0967726e75a8ad
negi-saurabh <saurabh.negi89@gmail.com>
2022-04-23 11:12:17 +0200

    23 Apr

---
+++ Practice/src/datastructures/linkedlist/InsertNodeatTheTail.java
@@ -0,0 +1,72 @@
+package datastructures.linkedlist;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class InsertNodeatTheTail {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	static class SinglyLinkedList {
+		public SinglyLinkedListNode head;
+
+		public SinglyLinkedList() {
+			this.head = null;
+		}
+
+	}
+
+	static class SinglyLinkedListNode {
+		public int data;
+		public SinglyLinkedListNode next;
+
+		public SinglyLinkedListNode(int nodeData) {
+			this.data = nodeData;
+			this.next = null;
+		}
+	}
+
+	public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+		while (node != null) {
+			bufferedWriter.write(String.valueOf(node.data));
+
+			node = node.next;
+
+			if (node != null) {
+				bufferedWriter.write(sep);
+			}
+		}
+	}
+
+	static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+		
+		if (head == null)
+			return head;
+		return head;
+
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+		SinglyLinkedList llist = new SinglyLinkedList();
+
+		int llistCount = scanner.nextInt();
+
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		for (int i = 0; i < llistCount; i++) {
+			int llistItem = scanner.nextInt();
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+			SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+			llist.head = llist_head;
+		}
+		printSinglyLinkedList(llist.head, "\n", bufferedWriter);
+		bufferedWriter.newLine();
+		bufferedWriter.close();
+		scanner.close();
+	}
+}
+
+
