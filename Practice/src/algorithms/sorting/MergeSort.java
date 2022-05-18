cd48a329eac5597d6d0f07b848ad4cbff0b3a1f4
negi-saurabh <saurabh.negi89@gmail.com>
2022-05-18 18:06:57 +0200

    18/05

--- Practice/src/algorithms/sorting/MergeSort.java
+++ Practice/src/algorithms/sorting/MergeSort.java
@@ -3,7 +3,50 @@ package algorithms.sorting;
 public class MergeSort {
 	
 	private void merge(int[] arr, int l, int m, int r) {
+		
+		//getting size of two subarrays
+		int n1 = m-l+1;
+		int n2 = r -m ;
+		
+		int[] arr1 = new int[n1];
+		int[] arr2 = new int[n2];
+		
+		for (int i = 0; i < n1; i++) {
+			arr1[i] = arr[l+i];
+		}
+		for (int i = 0; i < n2; i++) {
+			arr2[i] = arr[m+1+i];
+		}
 
+		// merge array
+		// Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+ 
+        // Initial index of merged subarray array
+        int k = l;
+        
+        while(i<n1 && j<n2) {
+        	if(arr1[i] <= arr2[j]) {
+        		arr[k] = arr1[i];
+        		i++;
+        	}else {
+        		arr[k] = arr2[j];
+        		j++;
+        	}
+        	k++;
+        }
+        
+        //copy remaining elements of arr1 and arr2
+        while(i < n1) {
+        	arr[k] = arr1[i];
+        	i++;
+        	k++;
+        }
+        while(j<n2) {
+        	arr[k] = arr2[j];
+        	j++;
+        	k++;
+        }
 	}
 
 	public void sort(int arr[], int l, int r)
