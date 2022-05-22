63db6b8081cad7bafeed8c2166b8904b35885d9e
negi-saurabh <saurabh.negi89@gmail.com>
2022-05-22 21:28:04 +0200

    22/05 part 3

--- Practice/src/leetcode/easy/linkedList/MergeTwoSortedList.java
+++ Practice/src/leetcode/easy/linkedList/MergeTwoSortedList.java
@@ -1,15 +1,32 @@
 package leetcode.easy.linkedList;
 
 public class MergeTwoSortedList {
-	
-	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-		
-		if(list1.val>list2.val) {
-			
-		}
-		return list2;
-	}
 
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		ListNode mergedList  = new ListNode();
+		while(list1 != null && list2 != null) {
+			if(list1.val < list2.val) {
+				mergedList.val = list1.val;
+				list1 = list1.next;
+			}else {
+				mergedList.val = list2.val;
+				list2 = list2.next;
+			}
+			mergedList = mergedList.next;
+		}
+
+		while(list1 != null) {
+			mergedList.val = list1.val;
+			list1 = list1.next;
+			mergedList = mergedList.next;
+		}
+		while(list2 != null) {
+			mergedList.val = list2.val;
+			list2 = list2.next;
+			mergedList = mergedList.next;
+		}
+		return mergedList;
+	}
 }
 
 class ListNode { 
