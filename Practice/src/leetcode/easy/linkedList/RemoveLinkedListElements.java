688cb5504da832c0df439459ada262910b5cc2f3
negi-saurabh <saurabh.negi89@gmail.com>
2022-05-25 08:07:23 +0200

    25/05

--- Practice/src/leetcode/easy/linkedList/RemoveLinkedListElements.java
+++ Practice/src/leetcode/easy/linkedList/RemoveLinkedListElements.java
@@ -3,29 +3,43 @@ package leetcode.easy.linkedList;
 public class RemoveLinkedListElements {
 
 	public static ListNode removeElements(ListNode head, int val) {
-		
-		while(head.next != null) {
-			if(head.next.val == val) {
-				head = head.next.next;
+
+		ListNode dummy = new ListNode(-1) , prev = dummy , toDelete;
+		dummy.next = head;
+		while(head != null) {
+			if(head.val == val) {
+				prev.next = head.next;
+			}
+			else {
+				prev = head;
 			}
 			head = head.next;
 		}
+		return dummy.next;
 
-		return head;
+		/*
+		 * if(head.val == val) { head = head.next; } ListNode temp = head;
+		 * while(head.next != null) { if(head.next.val == val) { head.next =
+		 * head.next.next; }else { head = head.next; } }
+		 * 
+		 * return temp;
+		 */
 	}
 
 
 	public static void main(String[] args) {
 		ListNode head = new ListNode(20);
 
-		head.next = new ListNode(4);
+		head.next = new ListNode(20);
 		head.next.next = new ListNode(5);
 		head.next.next.next  = new ListNode(6);
 		head.next.next.next.next  = new ListNode(10);
 		head.next.next.next.next.next  = new ListNode(6);
+		head.next.next.next.next.next.next  = new ListNode(20);
+		head.next.next.next.next.next.next.next  = new ListNode(20);
 
-		removeElements(head, 6);
-		printList(head);
+		ListNode temp = removeElements(head, 20);
+		printList(temp);
 	}
 
 
@@ -34,6 +48,6 @@ public class RemoveLinkedListElements {
 			System.out.println("value:" +  head.val);
 			head = head.next;
 		}
-		
+
 	}
 }
