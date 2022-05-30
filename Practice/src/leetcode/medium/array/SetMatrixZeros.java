9af12118fa6a0b74b26191d9309dfbef0bbc6c55
negi-saurabh <saurabh.negi89@gmail.com>
2022-05-30 22:44:17 +0200

    30/05

---
+++ Practice/src/leetcode/medium/array/SetMatrixZeros.java
@@ -0,0 +1,25 @@
+package leetcode.medium.array;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SetMatrixZeros {
+
+	public static void setZeroes(int[][] matrix) {
+
+	}
+
+
+
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int[][] intInput = {
+				{1, 1, 1},
+				{1, 0, 1},
+				{1, 1, 1}
+		};
+
+		setZeroes(intInput);
+	}
+}
