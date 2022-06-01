c5381b49effc7518ee4a388ef018e4d9b320ce1b
negi-saurabh <saurabh.negi89@gmail.com>
2022-06-01 06:48:43 +0200

    01/06

--- Practice/src/leetcode/medium/array/SetMatrixZeros.java
+++ Practice/src/leetcode/medium/array/SetMatrixZeros.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class SetMatrixZeros {
@@ -11,7 +12,7 @@ public class SetMatrixZeros {
 	public static void setZeroes(int[][] matrix) {
 		List<Integer> rowZeros = new ArrayList();
 		List<Integer> columnZeros = new ArrayList();
-		
+
 		for (int i = 0; i < matrix.length; i++) {
 			int[] row = matrix[i];
 			for (int j = 0; j < row.length; j++) {
@@ -22,9 +23,18 @@ public class SetMatrixZeros {
 			}
 		}
 
+		for (int i = 0; i < columnZeros.size(); i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[j][columnZeros.get(i)]=0;
+			}
+		}
+
+		for (int i = 0; i < rowZeros.size(); i++) {
+			for (int j = 0; j < matrix[rowZeros.get(i)].length; j++) {
+				matrix[rowZeros.get(i)][j]=0;
+			}
+		}
 	}
-
-
 
 	public static void main(String[] args) {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
---
+++ Practice/src/leetcode/medium/array/SortColors.java
@@ -0,0 +1,45 @@
+package leetcode.medium.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortColors {
+
+	public static void sortColors(int[] nums) {
+		if(nums == null || nums.length ==0)
+			return;
+		int zeros = 0;
+		int ones = 0;
+		int twos = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[i] == 0) {
+				zeros++;
+			}
+			if(nums[i] == 1) {
+				ones++;
+			}
+			if(nums[i] == 2) {
+				twos++;
+			}
+		}
+		
+		for (int i = 0; i < nums.length; i++) {
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = reader.readLine().split(",");
+		int[] intInput = new int[input.length];
+		for (int i = 0; i < input.length; i++) {
+			intInput[i] = Integer.parseInt(input[i]); 
+		}
+		sortColors(intInput);
+
+	}
+
+}
