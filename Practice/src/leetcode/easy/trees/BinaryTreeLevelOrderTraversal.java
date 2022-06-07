3e26b34bd3ee6c0d02a327ee97794fd76baa4b75
negi-saurabh <saurabh.negi89@gmail.com>
2022-06-07 21:54:29 +0200

    07/06 part 2

---
+++ Practice/src/leetcode/easy/trees/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,30 @@
+package leetcode.easy.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal {
+
+	List<List<Integer>> levelOrderlist = new ArrayList<>();
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<Integer> listPerLevel = new ArrayList<Integer>();
+		
+		if( root == null)
+			return levelOrderlist;
+		
+		listPerLevel.add(root.val);
+		
+		return null;
+	}
+
+	public static void main(String[] args) {
+		TreeNode leftnode = new TreeNode(3, new TreeNode(45) , new TreeNode(62));
+		TreeNode rightnode = new TreeNode(71, new TreeNode(89) , new TreeNode(22));
+		TreeNode node = new TreeNode(56, leftnode , rightnode);
+		List<List<Integer>>  orderedList = new BinaryTreeLevelOrderTraversal().levelOrder(node);
+		for (int i = 0; i < orderedList.size(); i++) {
+			System.out.println(orderedList.get(i));
+		}
+	}
+
+}
