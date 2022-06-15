d190d14c210692cb803fec632dd59db8f5dfb400
negi-saurabh <saurabh.negi89@gmail.com>
2022-06-15 07:55:48 +0200

    15/06 part 2

--- Practice/src/leetcode/easy/array/RemoveElement.java
+++ Practice/src/leetcode/easy/array/RemoveElement.java
@@ -1,5 +1,30 @@
 package leetcode.easy.array;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[i] != val) {
+				nums[count] = nums[i]; 
+				count++;
+			}
+		}
+		return count;
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = reader.readLine().split(",");
+		int[] intInput = new int[input.length];
+		for (int i = 0; i < input.length; i++) {
+			intInput[i] = Integer.parseInt(input[i]); 
+		}
+
+		System.out.println(new RemoveElement().removeElement(intInput,3));
+	}
+
 
 }
