450e0e3d01dd74cc7e799bac34fa5b195316307b
negi-saurabh <saurabh.negi89@gmail.com>
2023-01-12 15:45:27 +0100

    refactoring 2

--- Practice/src/leetcode/medium/array/KDivisibleElementsSubarrays.java
+++ Practice/src/leetcode/medium/array/KDivisibleElementsSubarrays.java
@@ -3,20 +3,13 @@ package leetcode.medium.array;
 import static java.util.stream.Collectors.toList;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
-
-import hackerRank.arrays.LeftRotationArray;
 
 public class KDivisibleElementsSubarrays {
 
