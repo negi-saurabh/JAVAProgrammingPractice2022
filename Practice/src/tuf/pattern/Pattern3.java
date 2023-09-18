b697771f7b9c0af552717c0b0650b1d05d8fbcfa
negi-saurabh <saurabh.negi89@gmail.com>
2023-09-18 08:56:14 +0200

    18 Sep

---
+++ Practice/src/tuf/pattern/Pattern3.java
@@ -0,0 +1,14 @@
+package tuf.pattern;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1 ; i<n+1 ; i++){
+            for(int j=0 ; j < i  ; j++){
+                System.out.print(j+1);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Practice/src/tuf/pattern/Pattern4.java
@@ -0,0 +1,14 @@
+package tuf.pattern;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1 ; i<n+1 ; i++){
+            for(int j=0 ; j < i  ; j++){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
