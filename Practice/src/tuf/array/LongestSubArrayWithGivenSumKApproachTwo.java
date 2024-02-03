14020c596e4f9ca74769915bc2498df5ba73f4dc
negi-saurabh <saurabh.negi89@gmail.com>
2024-02-03 17:45:34 +0100

    3rd feb

--- Practice/src/tuf/array/LongestSubArrayWithGivenSumKApproachTwo.java
+++ Practice/src/tuf/array/LongestSubArrayWithGivenSumKApproachTwo.java
@@ -6,24 +6,28 @@ import java.util.Map;
 import static java.lang.Math.max;
 
 public class LongestSubArrayWithGivenSumKApproachTwo {
-    public static void main(String[] args) {
-        int[] input = new int[]{1, 2, 3, 1, 1, 1, 1};
-        int sum = 3;
-        int tempSum = 0;
-        int maxLength = 0 ;
-        Map<Integer, Integer> sumIndexMap = new HashMap<>();
-        for (int i = 0; i < input.length; i++) {
-             tempSum =+ input[i];
-             if(sum == tempSum){
-                 maxLength= max(maxLength, i+1);
-             }
-             int rem = tempSum - sum;
-             if(sumIndexMap.containsKey(rem)){
-                    int len = i-sumIndexMap.get(rem);
-                    maxLength= max(maxLength, len);
-             }
-            sumIndexMap.put(tempSum, i);
+    public static int lenOfLongSubarr(int A[], int N, int K) {
+        //Complete the function
+        int sum = A[0];
+        int maxLength = 0;
+        if(sum == K){
+            maxLength = 1;
         }
-        System.out.println("Length is "+ maxLength );
+        int count = 1;
+        for(int i = 1 ; i < A.length ; i++){
+            sum += A[i];
+            count ++;
+            if(sum == K){
+                maxLength = Math.max(maxLength, count);
+                sum = 0;
+                count = 0;
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{1, 4, 3, 3, 5, 5};
+        new LongestSubArrayWithGivenSumKApproachTwo().lenOfLongSubarr(input, 6, 16);
     }
 }
