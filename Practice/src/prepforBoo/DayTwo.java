575e6e88f1f7ae82689b22d9f8db8b340f37276c
negi-saurabh <saurabh.negi89@gmail.com>
2026-08-06 19:56:21 +0200

    6 aug part 3

--- Practice/src/prepforBoo/DayTwo.java
+++ Practice/src/prepforBoo/DayTwo.java
@@ -9,10 +9,17 @@ public class DayTwo {
 
     public static boolean isPalindrome(String s) {
         // your code
-        char[] charArray =s.toCharArray();
-        int j= charArray.length-1;
-        for(int i = 0; i > j;i++ ){
-            if(charArray[i] != charArray[j]){
+        char[] charArray =s.toLowerCase().toCharArray();
+        StringBuffer  buffer = new StringBuffer();
+        for(int i = 0; i < charArray.length;i++ ) {
+                if(Character.isLetterOrDigit(charArray[i])){
+                    buffer.append(charArray[i]);
+            }
+        }
+        String cleanedString = buffer.toString();
+        int j= cleanedString.length()-1;
+        for(int i = 0; i < j;i++ ){
+            if(cleanedString.toCharArray()[i] != cleanedString.toCharArray()[j]){
                 return false;
             }
             j--;
