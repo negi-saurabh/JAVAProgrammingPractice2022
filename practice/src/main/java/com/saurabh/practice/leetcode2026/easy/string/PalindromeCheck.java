1b6620bfa967b129d7b7c3177fc5fe02ed8645f5
negi-saurabh <saurabh.negi89@gmail.com>
2026-09-22 12:20:33 +0200

    22 sep 3

--- practice/src/main/java/com/saurabh/practice/leetcode2026/easy/string/PalindromeCheck.java
+++ practice/src/main/java/com/saurabh/practice/leetcode2026/easy/string/PalindromeCheck.java
@@ -4,30 +4,29 @@ import com.saurabh.practice.leetcode2026.easy.array.MergeSortedArray;
 
 public class PalindromeCheck {
 
-        public boolean isPalindrome(String s) {
-            StringBuilder builder = new StringBuilder();
-
-            for(int i = 0; i < s.length() ; i++){
-                if(Character.isLetterOrDigit(s.charAt(i))){
-                    builder.append(s.charAt(i));
-                }
-            }
-
-            String cleanString  = builder.toString().toLowerCase();
-            if(cleanString.length() == 0)
-                return true;
-
-            int j = cleanString.length()-1;
-            int mid = j/2;
-
-            for(int i = 0; i <= mid ; i++){
-                if(cleanString.charAt(i) != cleanString.charAt(j)){
-                    return false;
-                }
-                j--;
-            }
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
             return true;
         }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst )) {
+                start++;
+            } else if(!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
 
